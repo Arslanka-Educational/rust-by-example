966e075f2a42f8636768b29a3f44cc5251a9bbb1
Arslan Giniiatullin <arslan-gin@yandex-team.ru>
2023-09-24 10:46:37 +0300

    Added fmt::Debug and fmt:Display impls in hello-world

--- rust-basics/hello-world/src/hello-world.rs
+++ rust-basics/hello-world/src/hello-world.rs
@@ -1,4 +1,24 @@
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8
+}
+
+use std::fmt;
+
+impl<'a> fmt::Display for Person<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.name, self.age)
+    }
+}
+
 fn main() {
-    println!("Hello world!");
-    println!("I am programming in Rust!")
+    let name = "Arslan";
+    let age = 19;
+    let person = Person { name, age };
+
+    // Pretty print Debug
+    println!("{:#?}", person);
+    // Pretty print Display
+    println!("{}", person);
 }

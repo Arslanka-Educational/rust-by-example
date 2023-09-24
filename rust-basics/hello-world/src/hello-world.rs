3fd4560c9dda391d9effe694420fef9a73831dba
Arslan Giniiatullin <arslan-gin@yandex-team.ru>
2023-09-24 11:29:07 +0300

    Added fmt::Display for Vec<>

--- rust-basics/hello-world/src/hello-world.rs
+++ rust-basics/hello-world/src/hello-world.rs
@@ -1,10 +1,10 @@
+use std::fmt;
+
 #[derive(Debug)]
 struct Person<'a> {
     name: &'a str,
-    age: u8
+    age: u8,
 }
-
-use std::fmt;
 
 impl<'a> fmt::Display for Person<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -12,13 +12,36 @@ impl<'a> fmt::Display for Person<'a> {
     }
 }
 
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}", v)?;
+        }
+
+        write!(f, "]")
+    }
+}
+
+
 fn main() {
     let name = "Arslan";
     let age = 19;
     let person = Person { name, age };
 
-    // Pretty print Debug
+    // Pretty print person with Debug
     println!("{:#?}", person);
-    // Pretty print Display
+    // Pretty print person with Display
     println!("{}", person);
+
+    let list = List(vec![1, 2, 3]);
+
+    //Pretty print Vec with Display
+    println!("{}", list);
 }
